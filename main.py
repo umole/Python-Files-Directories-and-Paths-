b01813898d6d3b97c1c83e22a1ab6021d9341ceb
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-09-11 05:41:02 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# with open("my_file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+
+with open("../../Desktop/my_file.txt", mode="w") as file:
+    file.write("\nThis is a new Text ")
+    # content = file.read()
+    # print(content)
+
+
