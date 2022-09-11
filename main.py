a7a1848a3d652b1e73d16035fcd7380024b2892e
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-09-11 05:46:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# with open("my_file.txt") as file:
-#     content = file.read()
-#     print(content)
+with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter:
+    letter_content = letter.read()
 
-
-with open("../../Desktop/my_file.txt", mode="w") as file:
-    file.write("\nThis is a new Text ")
-    # content = file.read()
-    # print(content)
-
-
+with open("../Mail Merge Project Start/Input/Names/invited_names.txt") as name_files:
+    names = name_files.readlines()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace("[name]", stripped_name)
+        with open(f"./Output/ReadyToSend/Letter To {stripped_name}", mode="w") as Completed_letter:
+            final_letter = Completed_letter.write(new_letter)
